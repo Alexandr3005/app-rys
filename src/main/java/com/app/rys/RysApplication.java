944299cd47fbfdf9018da9edb511b1004d286f19
command <command>
2023-03-11 12:12:29 +0100

    prueba

--- src/main/java/com/app/rys/RysApplication.java
+++ src/main/java/com/app/rys/RysApplication.java
@@ -16,6 +16,8 @@ import com.app.rys.repository.BuildingRepository;
 import com.app.rys.repository.UserRepository;
 
 @SpringBootApplication
+
+
 public class RysApplication {
 	
 	@Autowired
@@ -56,6 +58,7 @@ public class RysApplication {
 		Floor floor6 = new Floor();
 		Floor floor7 = new Floor();
 		Floor floor8 = new Floor();
+		Floor floor9 = new Floor(); //cambio prueba
 		
 		// 16 asientos num 1 y 2 de cada planta
 		// seat sin darle estado por haber hecho prePersist
