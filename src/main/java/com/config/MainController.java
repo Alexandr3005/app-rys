60ec4c87cd8be80ac51781a46f001b2a742dbbf1
Alexandr3005 <Alexandergsme@gmail.com>
2023-06-03 17:06:21 +0200

    solucion conflictos2

--- src/main/java/com/config/MainController.java
+++ src/main/java/com/config/MainController.java
@@ -3,13 +3,15 @@
 package com.config;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class MainController {
 
     @RequestMapping(value = {"/", "/{path:[^\\.]*}"})
-    public String forward() {
+    public String forward(@PathVariable(required = false) String path) {
         return "forward:/index.html";
     }
 }
